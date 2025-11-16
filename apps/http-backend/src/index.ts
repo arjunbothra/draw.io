d0d204e5b9fdff2f1f0a84c9c8876b9c5dde4250
arjunbothra <arjunbothra78@gmail.com>
2025-11-16 16:57:58 +0530

    Initial Zod Setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,18 +1,34 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
+
+
 
 const app = express();
 
 app.post("/signup", (req, res)=>{
-
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect imputs"
+        })
+        return;
+    }
     res.json({
         roomId : 123
     })
 })
 
 app.post("/signin", (req, res)=>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect imputs"
+        })
+        return;
+    }
 
     const userId= 1;
     const token = jwt.sign({
@@ -26,7 +42,13 @@ app.post("/signin", (req, res)=>{
 
 app.post("/room", middleware, (req, res)=>{
     // db call
-    
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect imputs"
+        })
+        return;
+    }
     
     res.json({
         roomId : 123
