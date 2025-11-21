33cd08c3fe6bf4c7bf35207adb9c0e0f3ab09201
arjunbothra <arjunbothra78@gmail.com>
2025-11-21 19:16:00 +0530

    initial canvas logic added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -5,10 +5,11 @@ import { AuthRequest, middleware } from "./middleware";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
 import { prismaClient } from "@repo/db/client";
 import bcrypt from "bcrypt";
-
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/signup", async (req, res)=>{
     const parsedData = CreateUserSchema.safeParse(req.body); 
